cf51f0211d4506cbd0e9035137463956932a08a2
Andrew Choi <andchoi@linkedin.com>
2019-12-09 22:28:33 -0800

    initial commit

--- src/main/java/com/linkedin/kmf/services/configs/MultiClusterTopicManagementServiceConfig.java
+++ src/main/java/com/linkedin/kmf/services/configs/MultiClusterTopicManagementServiceConfig.java
@@ -13,22 +13,15 @@ import java.util.Map;
 import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 
-import static org.apache.kafka.common.config.ConfigDef.Range.atLeast;
-
 public class MultiClusterTopicManagementServiceConfig extends AbstractConfig {
-
   private static final ConfigDef CONFIG;
-
   public static final String TOPIC_CONFIG = CommonServiceConfig.TOPIC_CONFIG;
   public static final String TOPIC_DOC = CommonServiceConfig.TOPIC_DOC;
-
   public static final String PROPS_PER_CLUSTER_CONFIG = "topic.management.props.per.cluster";
   public static final String PROPS_PER_CLUSTER_DOC = "A map from cluster name to a TopicManagementService config for each monitored cluster";
-
   public static final String REBALANCE_INTERVAL_MS_CONFIG = "topic-management.rebalance.interval.ms";
   public static final String REBALANCE_INTERVAL_MS_DOC = "The gap in ms between the times the cluster balance on the "
-      + "monitor topic is checked.  Set this to a large value to disable automatic topic rebalance.";
-
+      + "monitor topic is checked. Set this to a large value to disable automatic topic rebalance.";
   public static final String PREFERRED_LEADER_ELECTION_CHECK_INTERVAL_MS_CONFIG = "topic-management.preferred.leader.election.check.interval.ms";
   public static final String PREFERRED_LEADER_ELECTION_CHECK_INTERVAL_MS_DOC = "The gap in ms between the times to check if preferred leader election"
       + " can be performed when requested during rebalance";
@@ -42,13 +35,13 @@ public class MultiClusterTopicManagementServiceConfig extends AbstractConfig {
       .define(REBALANCE_INTERVAL_MS_CONFIG,
               ConfigDef.Type.INT,
               1000 * 60 * 10,
-              atLeast(10),
+              org.apache.kafka.common.config.ConfigDef.Range.atLeast(10),
               ConfigDef.Importance.LOW,
               REBALANCE_INTERVAL_MS_DOC)
         .define(PREFERRED_LEADER_ELECTION_CHECK_INTERVAL_MS_CONFIG,
             ConfigDef.Type.LONG,
             1000 * 60 * 5,
-            atLeast(5),
+            org.apache.kafka.common.config.ConfigDef.Range.atLeast(5),
             ConfigDef.Importance.LOW, PREFERRED_LEADER_ELECTION_CHECK_INTERVAL_MS_DOC);
   }
 
