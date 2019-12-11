ce201ae3cc081e840c9449fc2e03e51d90f7d030
Andrew Choi <andchoi@linkedin.com>
2019-12-10 18:43:53 -0800

    Remove Static Import & Allow conciseness on Measurables using lamda expression (#179)
    * Remove Static wildcard Imports & Allow conciseness on Measurables using lamda expression 🎨
    Signed-off-by: Andrew Choi <andchoi@linkedin.com>

--- src/main/java/com/linkedin/kmf/partitioner/NewKMPartitioner.java
+++ src/main/java/com/linkedin/kmf/partitioner/NewKMPartitioner.java
@@ -9,13 +9,11 @@
  */
 package com.linkedin.kmf.partitioner;
 
-import static org.apache.kafka.common.utils.Utils.murmur2;
-
 public class NewKMPartitioner implements KMPartitioner {
 
   public int partition(String key, int partitionNum) {
     byte[] keyBytes = key.getBytes();
-    return toPositive(murmur2(keyBytes)) % partitionNum;
+    return toPositive(org.apache.kafka.common.utils.Utils.murmur2(keyBytes)) % partitionNum;
   }
 
   private static int toPositive(int number) {
