ac73d65c271df8ba2bc6847776e156ad7a595c93
Andrew Choi <andchoi@linkedin.com>
2020-06-01 12:37:21 -0700

    Xinfra Monitor Rebranding Operation from Kafka Monitor  Signed-off-by: Andrew Choi <li_andchoi@microsoft.com>
    Xinfra Monitor Rebranding Operation from Kafka Monitor
    
    Signed-off-by: Andrew Choi <li_andchoi@microsoft.com>

--- bin/windows/kafka-monitor-start.bat
+++ bin/windows/kafka-monitor-start.bat
@@ -19,7 +19,7 @@ IF [%1] EQU [] (
 	EXIT /B 1
 )
 
-set COMMAND=%BASE_DIR%\kmf-run-class.bat com.linkedin.kmf.KafkaMonitor %*
+set COMMAND=%BASE_DIR%\kmf-run-class.bat com.linkedin.kmf.XinfraMonitor %*
 
 rem echo basedir: %BASE_DIR%
 
