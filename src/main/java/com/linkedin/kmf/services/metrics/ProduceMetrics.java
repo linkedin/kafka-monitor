35d51a3d7fa699d033e755e0c6c8eb96b269765c
Andrew Choi <li_andchoi@microsoft.com>
2020-06-15 02:40:17 -0700

    fix the PR testing
    Signed-off-by: Andrew Choi <li_andchoi@microsoft.com>

--- src/main/java/com/linkedin/kmf/services/metrics/ProduceMetrics.java
+++ src/main/java/com/linkedin/kmf/services/metrics/ProduceMetrics.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2020 LinkedIn Corp. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 package com.linkedin.kmf.services.metrics;
 
 import com.linkedin.kmf.XinfraMonitorConstants;
@@ -70,44 +80,46 @@ public class ProduceMetrics {
     metrics.addMetric(
         new MetricName("produce-availability-avg", XinfraMonitorConstants.METRIC_GROUP_NAME_PRODUCE_SERVICE,
             "The average produce availability", tags), (config, now) -> {
-          double availabilitySum = 0.0;
-          int partitionNum = partitionNumber.get();
-          for (int partition = 0; partition < partitionNum; partition++) {
-            double recordsProduced = (double) metrics.metrics()
-                .get(metrics.metricName("records-produced-rate-partition-" + partition,
-                    XinfraMonitorConstants.METRIC_GROUP_NAME_PRODUCE_SERVICE, tags))
-                .metricValue();
-            double produceError = (double) metrics.metrics()
-                .get(metrics.metricName("produce-error-rate-partition-" + partition,
-                    XinfraMonitorConstants.METRIC_GROUP_NAME_PRODUCE_SERVICE, tags))
-                .metricValue();
-            // If there is no error, error rate sensor may expire and the value may be NaN. Treat NaN as 0 for error rate.
-            if (Double.isNaN(produceError) || Double.isInfinite(produceError)) {
-              produceError = 0;
-            }
-            // If there is either succeeded or failed produce to a partition, consider its availability as 0.
-            if (recordsProduced + produceError > 0) {
-              availabilitySum += recordsProduced / (recordsProduced + produceError);
-            } else if (!treatZeroThroughputAsUnavailable) {
-              // If user configures treatZeroThroughputAsUnavailable to be false, a partition's availability
-              // is 1.0 as long as there is no exception thrown from producer.
-              // This allows kafka admin to exactly monitor the availability experienced by Kafka users which
-              // will block and retry for a certain amount of time based on its configuration (e.g. retries, retry.backoff.ms).
-              // Note that if it takes a long time for messages to be retries and sent, the latency in the ConsumeService
-              // will increase and it will reduce ConsumeAvailability if the latency exceeds consume.latency.sla.ms
-              // If timeout is set to more than 60 seconds (the current samples window duration),
-              // the error sample might be expired before the next error can be produced.
-              // In order to detect offline partition with high producer timeout config, the error status during last
-              // send is also checked before declaring 1.0 availability for the partition.
-              Boolean lastSendError = _produceErrorInLastSendPerPartition.get(partition);
-              if (lastSendError == null || !lastSendError) {
-                availabilitySum += 1.0;
-              }
+        double availabilitySum = 0.0;
+        int partitionNum = partitionNumber.get();
+        for (int partition = 0; partition < partitionNum; partition++) {
+          double recordsProduced = (double) metrics.metrics()
+              .get(metrics.metricName("records-produced-rate-partition-" + partition,
+                  XinfraMonitorConstants.METRIC_GROUP_NAME_PRODUCE_SERVICE, tags))
+              .metricValue();
+          double produceError = (double) metrics.metrics()
+              .get(metrics.metricName("produce-error-rate-partition-" + partition,
+                  XinfraMonitorConstants.METRIC_GROUP_NAME_PRODUCE_SERVICE, tags))
+              .metricValue();
+          // If there is no error, error rate sensor may expire and the value may be NaN. Treat NaN as 0 for error rate.
+          if (Double.isNaN(produceError) || Double.isInfinite(produceError)) {
+            produceError = 0;
+          }
+          // If there is either succeeded or failed produce to a partition, consider its availability as 0.
+          if (recordsProduced + produceError > 0) {
+            availabilitySum += recordsProduced / (recordsProduced + produceError);
+          } else if (!treatZeroThroughputAsUnavailable) {
+            // If user configures treatZeroThroughputAsUnavailable to be false, a partition's availability
+            // is 1.0 as long as there is no exception thrown from producer.
+            // This allows kafka admin to exactly monitor the availability experienced by Kafka users which
+            // will block and retry for a certain amount of time based on its configuration (e.g. retries, retry.backoff.ms).
+            // Note that if it takes a long time for messages to be retries and sent, the latency in the ConsumeService
+            // will increase and it will reduce ConsumeAvailability if the latency exceeds consume.latency.sla.ms
+            // If timeout is set to more than 60 seconds (the current samples window duration),
+            // the error sample might be expired before the next error can be produced.
+            // In order to detect offline partition with high producer timeout config, the error status during last
+            // send is also checked before declaring 1.0 availability for the partition.
+            Boolean lastSendError = _produceErrorInLastSendPerPartition.get(partition);
+            if (lastSendError == null || !lastSendError) {
+              availabilitySum += 1.0;
             }
           }
-          // Assign equal weight to per-partition availability when calculating overall availability
-          return availabilitySum / partitionNum;
-        });
+        }
+
+        // Assign equal weight to per-partition availability when calculating overall availability
+        return availabilitySum / partitionNum;
+      }
+    );
   }
 
   public void addPartitionSensors(int partition) {
