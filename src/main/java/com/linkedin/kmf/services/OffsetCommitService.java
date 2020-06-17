3d92f6b8fc62efc1048ec523fb2b7fc513994023
Andrew Choi <li_andchoi@microsoft.com>
2020-06-17 11:50:51 -0700

    wip

--- src/main/java/com/linkedin/kmf/services/OffsetCommitService.java
+++ src/main/java/com/linkedin/kmf/services/OffsetCommitService.java
@@ -1,3 +1,13 @@
+/**
+ * Copyright 2020 LinkedIn Corp. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
 package com.linkedin.kmf.services;
 
 import org.apache.kafka.clients.consumer.internals.ConsumerNetworkClient;
@@ -7,7 +17,7 @@ import org.apache.kafka.common.utils.SystemTime;
 
 
 /**
- *
+ * Service that monitors the commit offset availability of a partiular Consumer Group.
  */
 public class OffsetCommitService implements Service {
 
